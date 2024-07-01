a7ae37089c53aae5da52162be17369ed4ba3e878
Empairim <titusfaulknertull@yahoo.com>
2024-07-01 18:38:27 -0400

    setting up assetkeys for neater code

---
+++ src/assets/asset-keys.js
@@ -0,0 +1,29 @@
+export const AssetKeys = Object.freeze({
+     PLAYER: {
+        //Movement
+         MOVEMENT: {
+            DASH: 'assets/player/movement/Dash.png',
+            DIE: 'assets/player/movement/Die.png',
+            IDLE: 'assets/player/movement/Idle.png',
+            JUMPANDFALL: 'assets/player/movement/JumpAndFall.png',
+            RUN: 'assets/player/movement/Run.png',
+            TAKEHIT: 'assets/player/movement/TakeHit.png',
+            // Add more movement assets here
+        },
+        //Air Attack
+        AIR_ATTACK1: 'assets/player/airAttack/AirCombo1.png',
+        AIR_ATTACK2: 'assets/player/airAttack/AirCombo2.png',
+        AIR_ATTACK3: 'assets/player/airAttack/AirCombo3.png',
+        SHOOTGUN: 'assets/player/airAttack/ShootGun1.png',
+        //Attack
+        ATTACK: {
+            ATTACK1: 'assets/player/attack/GroundCombo1.png',
+            ATTACK2: 'assets/player/attack/GroundCombo2.png',
+            ATTACK3: 'assets/player/attack/GroundCombo3.png',
+            LUNGINGSTAB: 'assets/player/attack/LungingStab.png',
+            SHOOTGUN: 'assets/player/attack/ShootGun1.png',
+        }
+        // Add more player assets here
+    },
+    // Add more asset categories here
+});
